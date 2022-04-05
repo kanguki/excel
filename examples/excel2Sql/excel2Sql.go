d0fb9775f523076d6a5f020692f522f700eb78de
Mo Nguyen <mo.nguyen@difisoft.com>
2022-04-05 15:09:10 +0700

    change code layout :D

--- examples/excel2Sql/excel2Sql.go
+++ examples/excel2Sql/excel2Sql.go
@@ -6,12 +6,13 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/kanguki/doExcel"
 )
 
 var (
-	table          = getEnv(TABLE_NAME, "`tradex-user-utility`.`t_favorite_list_test`")
+	table          = getEnv(TABLE_NAME, "`tradex-user-utility`.`t_favorite_list`")
 	excelFilePath  = getEnv(EXCEL_FILE_PATH, "doc/test2_out.xlsx")
 	excelFileSheet = getEnv(EXCEL_FILE_SHEET, "Sheet1")
 	outPath        = getEnv(SQL_OUT_PATH, "doc/test_out.sql")
@@ -28,52 +29,57 @@ func Do() {
 		doExcel.Log("error reading xlsx file: %v", err)
 		os.Exit(1)
 	}
-	limitNGoroutines := make(chan interface{}, 100000)
-	result := make(chan string)
-	for _, v := range data {
-		limitNGoroutines <- struct{}{}
-		go convert(limitNGoroutines, result, v)
-	}
-
-	//open file once
-	if err := os.Truncate(outPath, 0); err != nil {
-		doExcel.Debug("Failed to truncate: %v", err)
-	}
-
-	f, err := os.OpenFile(outPath,
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		doExcel.Log("error tructcate text file %v", err)
-	}
-	defer f.Close()
-
+	result := make(chan string) //increase channel capacity doesn't really improve much :D
+	countSuccess := make(chan int, 1)
+	countSuccess <- 0
+	var wg sync.WaitGroup
+	wg.Add(1)
 	//process out data
-	batch, countSuccess := []string{}, 0
-	for i := 0; i < len(data); i++ {
-		info := <-result
-		if info != "" {
-			// insert multiple records each time
-			batch = append(batch, info)
-			if len(batch) == insertBatch || i == (len(data)-1) {
-				w := "INSERT INTO " + table + " (`name`, `username`, `order`, `count`, `max_count`, `symbol_list`, `created_at`, `updated_at`) VALUES " + strings.Join(batch, ",") + ";\n"
-				if _, err := f.WriteString(w); err != nil {
-					doExcel.Debug("error write data to sql file: %v", err)
-				}
-				batch = []string{}
-			}
-			countSuccess++
+	go func() {
+		defer wg.Done()
+		//open file once
+		if err := os.Truncate(outPath, 0); err != nil {
+			doExcel.Debug("Failed to truncate: %v", err)
 		}
+
+		f, err := os.OpenFile(outPath,
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			doExcel.Log("error tructcate text file %v", err)
+		}
+		defer f.Close()
+
+		batch := []string{}
+		for i := 0; i < len(data); i++ {
+			info := <-result
+			if info != "" {
+				// insert multiple records each time
+				batch = append(batch, info)
+				if len(batch) == insertBatch || i == (len(data)-1) {
+					w := "INSERT INTO " + table + " (`name`, `username`, `order`, `count`, `max_count`, `symbol_list`, `created_at`, `updated_at`) VALUES " + strings.Join(batch, ",") + ";\n"
+					if _, err := f.WriteString(w); err != nil {
+						doExcel.Debug("error write data to sql file: %v", err)
+					}
+					batch = []string{}
+				}
+				countSuccess <- <-countSuccess + 1
+			}
+		}
+	}()
+	for _, v := range data {
+		convert(result, v) //no need to use goroutine here. it's not parallel.
 	}
-	doExcel.Log("Handle %v records", countSuccess)
+	wg.Wait()
+
+	doExcel.Log("Handle %v records", <-countSuccess)
 }
 
 //data that has inconstent format are skipped
 //wg is to close chan in, != limitNGoroutines
-func convert(limitNGoroutines chan interface{}, in chan string, data []string) {
+func convert(in chan string, data []string) {
 	var info string
 	defer func() {
 		in <- info
-		<-limitNGoroutines
 	}()
 	l := len(data)
 	if l < 4 { //4 when list is empty
@@ -96,7 +102,7 @@ func convert(limitNGoroutines chan interface{}, in chan string, data []string) {
 	}
 	//no need to convert to struct first, but it's more readable
 	userId := sanitize(data[0], " ", "'", "\"", "\n")
-	name := sanitize(data[2], " ", "'", "\"", "\n")
+	name := sanitize(data[2], "'", "\"", "\n")
 	info = fmt.Sprintf("('%v', '%v', %v, %v, 50, '[%v]', now(), now())", name, userId, order, len(listStr), strings.Join(listStr, ","))
 }
 
@@ -105,7 +111,7 @@ func sanitize(s string, cutSets ...string) string {
 	for _, v := range cutSets {
 		s = strings.ReplaceAll(s, v, "")
 	}
-	return s
+	return strings.TrimSpace(s)
 }
 
 func isAStock(s string) (cleaned string, isStock bool) {
--- examples/excel2Sql/excel2Sql_test.go
+++ examples/excel2Sql/excel2Sql_test.go
@@ -6,6 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func BenchmarkDo(b *testing.B) {
+	//export SQL_OUT_PATH=
+	//export EXCEL_FILE_PATH=
+	for i := 0; i < b.N; i++ {
+		Do()
+	}
+}
+
 func TestIsAStock(t *testing.T) {
 	shouldBeTrue := []string{"AAA", "A32 ", "VN30F2010'"}
 	for _, v := range shouldBeTrue {
