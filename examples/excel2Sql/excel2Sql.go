31ea2b9f6f9bd93bd8e24e8e389c46be8d3b3a16
Mo Nguyen <mo.nguyen@difisoft.com>
2022-04-06 13:54:16 +0700

    use fan in fan out

--- examples/excel2Sql/excel2Sql.go
+++ examples/excel2Sql/excel2Sql.go
@@ -12,11 +12,13 @@ import (
 )
 
 var (
-	table          = getEnv(TABLE_NAME, "`tradex-user-utility`.`t_favorite_list`")
-	excelFilePath  = getEnv(EXCEL_FILE_PATH, "doc/test2_out.xlsx")
-	excelFileSheet = getEnv(EXCEL_FILE_SHEET, "Sheet1")
-	outPath        = getEnv(SQL_OUT_PATH, "doc/test_out.sql")
-	insertBatch    = 50
+	table           = getEnv(TABLE_NAME, "`tradex-user-utility`.`t_favorite_list`")
+	excelFilePath   = getEnv(EXCEL_FILE_PATH, "doc/test2_out.xlsx")
+	excelFileSheet  = getEnv(EXCEL_FILE_SHEET, "Sheet1")
+	outPath         = getEnv(SQL_OUT_PATH, "doc/test_out.sql")
+	insertBatch     = 50
+	maxJobPerWorker = 100
+	numWorkers      = 5
 )
 
 //read excel file
@@ -24,86 +26,123 @@ var (
 //write to sql file
 func Do() {
 	printUsage()
+	data := getInputChan()
+	outs := []<-chan string{}
+	for i := 0; i < numWorkers; i++ {
+		outs = append(outs, convert(data))
+	}
+	result := mergeOutput(outs...)
+	countSuccess := make(chan int, 1)
+	countSuccess <- 0
+	//process out data
+	if err := os.Truncate(outPath, 0); err != nil {
+		doExcel.Debug("Failed to truncate: %v", err)
+	}
+
+	f, err := os.OpenFile(outPath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		doExcel.Log("error tructcate text file %v", err)
+	}
+	defer f.Close()
+
+	batch := []string{}
+	for info := range result {
+		if info != "" {
+			// insert multiple records each time
+			batch = append(batch, info)
+			if len(batch) == insertBatch || len(result) == 0 {
+				w := "INSERT INTO " + table + " (`name`, `username`, `order`, `count`, `max_count`, `symbol_list`, `created_at`, `updated_at`) VALUES " + strings.Join(batch, ",") + ";\n"
+				if _, err := f.WriteString(w); err != nil {
+					doExcel.Debug("error write data to sql file: %v", err)
+				}
+				batch = []string{}
+			}
+			countSuccess <- <-countSuccess + 1
+		}
+	}
+
+	doExcel.Log("Handle %v records", <-countSuccess)
+}
+func getInputChan() <-chan []string {
 	data, err := doExcel.ReadSheet(excelFilePath, excelFileSheet)
 	if err != nil {
 		doExcel.Log("error reading xlsx file: %v", err)
 		os.Exit(1)
 	}
-	result := make(chan string) //increase channel capacity doesn't really improve much :D
-	countSuccess := make(chan int, 1)
-	countSuccess <- 0
-	var wg sync.WaitGroup
-	wg.Add(1)
-	//process out data
+	input := make(chan []string, maxJobPerWorker)
+
 	go func() {
-		defer wg.Done()
-		//open file once
-		if err := os.Truncate(outPath, 0); err != nil {
-			doExcel.Debug("Failed to truncate: %v", err)
+		for _, d := range data {
+			input <- d
 		}
-
-		f, err := os.OpenFile(outPath,
-			os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			doExcel.Log("error tructcate text file %v", err)
-		}
-		defer f.Close()
-
-		batch := []string{}
-		for i := 0; i < len(data); i++ {
-			info := <-result
-			if info != "" {
-				// insert multiple records each time
-				batch = append(batch, info)
-				if len(batch) == insertBatch || i == (len(data)-1) {
-					w := "INSERT INTO " + table + " (`name`, `username`, `order`, `count`, `max_count`, `symbol_list`, `created_at`, `updated_at`) VALUES " + strings.Join(batch, ",") + ";\n"
-					if _, err := f.WriteString(w); err != nil {
-						doExcel.Debug("error write data to sql file: %v", err)
-					}
-					batch = []string{}
-				}
-				countSuccess <- <-countSuccess + 1
-			}
-		}
+		close(input)
 	}()
-	for _, v := range data {
-		convert(result, v) //no need to use goroutine here. it's not parallel.
-	}
-	wg.Wait()
 
-	doExcel.Log("Handle %v records", <-countSuccess)
+	return input
+}
+
+func mergeOutput(outs ...<-chan string) <-chan string {
+	var wg sync.WaitGroup
+	merged := make(chan string, maxJobPerWorker)
+	wg.Add(len(outs))
+
+	for _, o := range outs {
+		go func(c <-chan string) {
+			for data := range c {
+				merged <- data
+			}
+			wg.Done()
+		}(o)
+	}
+
+	go func() {
+		wg.Wait()
+		close(merged)
+	}()
+
+	return merged
 }
 
 //data that has inconstent format are skipped
 //wg is to close chan in, != limitNGoroutines
-func convert(in chan string, data []string) {
-	var info string
-	defer func() {
-		in <- info
-	}()
-	l := len(data)
-	if l < 4 { //4 when list is empty
-		fmt.Printf("incorrect data format :( %v\n", data)
-		return
-	}
-	order, err := strconv.Atoi(data[1])
-	if err != nil {
-		doExcel.Debug("error convert data (order) to info: %v\n", err)
-		return
-	}
-	var list, listStr []string
-	if l == 5 {
-		list = strings.Split(data[4], ",")
-		for _, v := range list {
-			if s, isStock := isAStock(v); isStock {
-				listStr = append(listStr, fmt.Sprintf("\\\"{\\\\\\\"data\\\\\\\":\\\\\\\"%v\\\\\\\",\\\\\\\"isNote\\\\\\\":false}\\\"", s))
+func convert(in <-chan []string) <-chan string {
+	out := make(chan string, maxJobPerWorker)
+	go func() {
+		for data := range in {
+			var info, userId, name string
+			var list, listStr []string
+			var order int
+			var err error
+			l := len(data)
+			if l < 4 { //4 when list is empty
+				fmt.Printf("incorrect data format :( %v\n", data)
+				goto end
 			}
+			order, err = strconv.Atoi(data[1])
+			if err != nil {
+				doExcel.Debug("error convert data (order) to info: %v\n", err)
+				goto end
+			}
+
+			if l == 5 {
+				list = strings.Split(data[4], ",")
+				for _, v := range list {
+					if s, isStock := isAStock(v); isStock {
+						listStr = append(listStr, fmt.Sprintf("\\\"{\\\\\\\"data\\\\\\\":\\\\\\\"%v\\\\\\\",\\\\\\\"isNote\\\\\\\":false}\\\"", s))
+					}
+				}
+			}
+			//no need to convert to struct first, but it's more readable
+			userId = sanitize(data[0], " ", "'", "\"", "\n")
+			name = sanitize(data[2], "'", "\"", "\n")
+			info = fmt.Sprintf("('%v', '%v', %v, %v, 50, '[%v]', now(), now())", name, userId, order, len(listStr), strings.Join(listStr, ","))
+		end:
+			out <- info
 		}
-	}
-	//no need to convert to struct first, but it's more readable
-	userId := sanitize(data[0], " ", "'", "\"", "\n")
-	name := sanitize(data[2], "'", "\"", "\n")
-	info = fmt.Sprintf("('%v', '%v', %v, %v, 50, '[%v]', now(), now())", name, userId, order, len(listStr), strings.Join(listStr, ","))
+		close(out)
+	}()
+	return out
 }
 
 //remove multiple characters in string
